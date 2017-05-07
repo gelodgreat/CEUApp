4c603654978e7098d5178809c148c03d2767e65a
andrewhamili <rockr1521@gmail.com>
2017-05-07 11:57:39 +0800

    WebCache for UniversityCalendar.

--- app/src/main/java/com/gelo/ceuapp/UnivCalendar.java
+++ app/src/main/java/com/gelo/ceuapp/UnivCalendar.java
@@ -3,7 +3,10 @@ package com.gelo.ceuapp;
 
 import android.content.Intent;
 import android.content.pm.ActivityInfo;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.os.Bundle;
+import android.renderscript.ScriptGroup;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.MenuItem;
@@ -12,6 +15,14 @@ import android.view.ViewGroup;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.widget.EditText;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URL;
+import java.net.URLConnection;
+
+import static android.content.Context.CONNECTIVITY_SERVICE;
 
 
 /**
@@ -49,12 +60,16 @@ public class UnivCalendar extends Fragment {
     }
 
 
+
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_univ_calendar, container, false);
 
         WebView wv = (WebView) view.findViewById(R.id.wv_uvc);
+        wv.getSettings().setCacheMode(WebSettings.LOAD_DEFAULT);
+        wv.getSettings().setAppCacheEnabled(true);
         WebSettings webSettings = wv.getSettings();
         webSettings.setBuiltInZoomControls(true);
         webSettings.setSupportZoom(true);
@@ -63,7 +78,28 @@ public class UnivCalendar extends Fragment {
         webSettings.setDisplayZoomControls(false);
         getActivity().setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
 
-        wv.loadUrl("file:///android_asset/Calendar.html");
+        Boolean net = ((MainActivity) getActivity()).isNetworkAvailable();
+        if (net==false) { // loading offline
+            wv.getSettings().setCacheMode( WebSettings.LOAD_CACHE_ELSE_NETWORK );
+        }
+
+        wv.loadUrl( "https://raw.githubusercontent.com/andrewhamili/CEUApp/master/app/src/main/assets/Calendar.html" );
+
+        /*String add="https://raw.githubusercontent.com/andrewhamili/CEUApp/master/app/src/main/AndroidManifest.xml";
+
+
+        try {
+            URLConnection cn=new URL(add).openConnection();
+            cn.connect();
+            InputStream stream =cn.getInputStream();
+
+            File download = new File(getContext().getCacheDir(), "AndroidManifest.xml");
+
+            wv.loadUrl();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }*/
 
 
         return view;
