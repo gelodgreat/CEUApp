52a459d5c16d916da8cfaf72114f7060835ebd7f
angelodgreat <carions46@gmail.com>
2017-05-11 12:49:57 +0800

    Changed Coding Format

--- app/src/main/java/com/gelo/ceuapp/ListBuildings.java
+++ app/src/main/java/com/gelo/ceuapp/ListBuildings.java
@@ -11,7 +11,7 @@ public class ListBuildings extends AppCompatActivity {
 
 
     ListView list;
-    String[] bldgname = {"DentSCI", "FGH", "ISC", "TECH CENTER", "SDVH", "FFH", "MVH", "DCTH", "PHLH", "FSH", "Student Activity Center", "North Quadrangle", "CDL", "Chapel", "LAH", "CAH", "GDLSC"
+    String[] bldgname = {"DentSci", "FGH", "ISC", "TECH CENTER", "SDVH", "FFH", "MVH", "DCTH", "PHLH", "FSH", "Student Activity Center", "North Quadrangle", "CDL", "Chapel", "LAH", "CAH", "GDLSC"
 
     };
     // FOR FUTURE USE IF WE ALREADY HAVE DESC FOR EACH BUILDINGS
@@ -40,43 +40,46 @@ public class ListBuildings extends AppCompatActivity {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
                 touched = list.getItemAtPosition(position).toString();
+                bldgcondition();
 
-                if (touched == "DentSCI") {
-                    startActivity(new Intent(ListBuildings.this, dent_sci.class));
-                } else if (touched == "FGH") {
-                    startActivity(new Intent(ListBuildings.this, fgh.class));
-                } else if (touched == "ISC") {
-                    startActivity(new Intent(ListBuildings.this, isc.class));
-                } else if (touched == "TECH CENTER") {
-                    startActivity(new Intent(ListBuildings.this, tech_center.class));
-                } else if (touched == "SDVH") {
-                    startActivity(new Intent(ListBuildings.this, sdvh.class));
-                } else if (touched == "FFH") {
-                    startActivity(new Intent(ListBuildings.this, ffh.class));
-                } else if (touched == "MVH") {
-                    startActivity(new Intent(ListBuildings.this, mvh.class));
-                } else if (touched == "DCTH") {
-                    startActivity(new Intent(ListBuildings.this, dcth.class));
-                } else if (touched == "PHLH") {
-                    startActivity(new Intent(ListBuildings.this, phlh.class));
-                } else if (touched == "Student Activity Center") {
-                    startActivity(new Intent(ListBuildings.this, sac.class));
-                } else if (touched == "North Quadrangle") {
-                    startActivity(new Intent(ListBuildings.this, north_quad.class));
-                } else if (touched == "CDL") {
-                    startActivity(new Intent(ListBuildings.this, cdl.class));
-                } else if (touched == "Chapel") {
-                    startActivity(new Intent(ListBuildings.this, chapel.class));
-                } else if (touched == "LAH") {
-                    startActivity(new Intent(ListBuildings.this, lah.class));
-                } else if (touched == "CAH") {
-                    startActivity(new Intent(ListBuildings.this, cah.class));
-                } else if (touched == "GDLSC") {
-                    startActivity(new Intent(ListBuildings.this, gdlsc.class));
-                }
             }
         });
     }
 
 
+    private void bldgcondition() {
+        if (touched == "DentSci") {
+            startActivity(new Intent(ListBuildings.this, dent_sci.class));
+        } else if (touched == "FGH") {
+            startActivity(new Intent(ListBuildings.this, fgh.class));
+        } else if (touched == "ISC") {
+            startActivity(new Intent(ListBuildings.this, isc.class));
+        } else if (touched == "TECH CENTER") {
+            startActivity(new Intent(ListBuildings.this, tech_center.class));
+        } else if (touched == "SDVH") {
+            startActivity(new Intent(ListBuildings.this, sdvh.class));
+        } else if (touched == "FFH") {
+            startActivity(new Intent(ListBuildings.this, ffh.class));
+        } else if (touched == "MVH") {
+            startActivity(new Intent(ListBuildings.this, mvh.class));
+        } else if (touched == "DCTH") {
+            startActivity(new Intent(ListBuildings.this, dcth.class));
+        } else if (touched == "PHLH") {
+            startActivity(new Intent(ListBuildings.this, phlh.class));
+        } else if (touched == "Student Activity Center") {
+            startActivity(new Intent(ListBuildings.this, sac.class));
+        } else if (touched == "North Quadrangle") {
+            startActivity(new Intent(ListBuildings.this, north_quad.class));
+        } else if (touched == "CDL") {
+            startActivity(new Intent(ListBuildings.this, cdl.class));
+        } else if (touched == "Chapel") {
+            startActivity(new Intent(ListBuildings.this, chapel.class));
+        } else if (touched == "LAH") {
+            startActivity(new Intent(ListBuildings.this, lah.class));
+        } else if (touched == "CAH") {
+            startActivity(new Intent(ListBuildings.this, cah.class));
+        } else if (touched == "GDLSC") {
+            startActivity(new Intent(ListBuildings.this, gdlsc.class));
+        }
+    }
 }
