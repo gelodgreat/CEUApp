d9758fb1d38300c2eb3dc0c3f1d3a4de0cfee5a8
angelodgreat <carions46@gmail.com>
2017-05-11 09:14:00 +0800

    Mock Up

---
+++ app/src/main/java/com/gelo/ceuapp/ListBuildings.java
@@ -0,0 +1,13 @@
+package com.gelo.ceuapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ListBuildings extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_list_buildings);
+    }
+}
