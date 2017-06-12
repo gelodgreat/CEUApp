5f0d54b63fae694858fd5e1ba1d40a3e4f462565
Angelo Umali <gelodgreat@users.noreply.github.com>
2017-06-12 13:47:52 +0800

    Finished Working for Search FAQ

--- app/src/main/java/com/gelo/ceuapp/fragments/fragment_search.java
+++ app/src/main/java/com/gelo/ceuapp/fragments/fragment_search.java
@@ -1,6 +1,7 @@
 package com.gelo.ceuapp.fragments;
 
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
@@ -13,11 +14,15 @@ import android.widget.ArrayAdapter;
 import android.widget.AutoCompleteTextView;
 
 import com.gelo.ceuapp.R;
+import com.gelo.ceuapp.faqfolder.ceuchoosemusic;
+import com.gelo.ceuapp.faqfolder.ceumusic;
+import com.gelo.ceuapp.faqfolder.enrollmentproc;
+import com.gelo.ceuapp.faqfolder.othertransac;
 
 
 public class fragment_search extends Fragment {
 
-    private final String[] faq_list = {"Board of Directors", "Board Members", "El Collar De Sampaguita", "Sampaguita La Flor De Manila", "Sampaguitang Mabango", "CEU Hymn", "CEU History", "Serious Offense", "Less Serious Offense", "Slight Offense", "Other Offenses", "Freshmen Enrollment", "Alien/Non-Resident Enrollment", "Second Degree Enrollment", "Continuing Student Enrollment", "Transferee Enrollment", "Enrollment Procedures", "Other Transactions", "Add and Drop", "Cross Enroll", "Evaluation for Graduating Students", "ID", "Promissory Note", "Re-Admission", "Readmission", "Refund", "Shifting"};
+    private final String[] faq_list = {"Board of Directors", "Board Members", "El Collar De Sampaguita", "Sampaguita La Flor De Manila", "Sampaguitang Mabango", "CEU Hymn", "CEU History", "Serious Offense", "CEU Music", "Less Serious Offense", "Slight Offense", "Other Offenses", "Freshmen Enrollment", "Alien/Non-Resident Enrollment", "Second Degree Enrollment", "Continuing Student Enrollment", "Transferee Enrollment", "Enrollment Procedures", "Other Transactions", "Add and Drop", "Cross Enroll", "Evaluation for Graduating Students", "ID", "Promissory Note", "Re-Admission", "Readmission", "Refund", "Shifting"};
 
     WebView wv_loadings_faq, wv_faq_search;
     AutoCompleteTextView autoCompleteTextView;
@@ -71,54 +76,93 @@ public class fragment_search extends Fragment {
         if (search.equalsIgnoreCase("Board of Directors") || search.equalsIgnoreCase("Board Members")) {
             wv_faq_search.loadUrl("file:///android_asset/board_of_directors.html");
             autoCompleteTextView.setText("");
-        } else if (search.equalsIgnoreCase("El Collar De Sampaguita")) {
-
-        } else if (search.equalsIgnoreCase("Sampaguita La Flor De Manila")) {
-
-        } else if (search.equalsIgnoreCase("Sampaguitang Mabango")) {
-
         } else if (search.equalsIgnoreCase("Enrollment Procedures")) {
-
+            Intent intent = new Intent(getActivity(), enrollmentproc.class);
+            startActivity(intent);
+            autoCompleteTextView.setText("");
         } else if (search.equalsIgnoreCase("Other Transactions")) {
-
-        } else if (search.equalsIgnoreCase("CEU Hymn")) {
-
+            Intent intent = new Intent(getActivity(), othertransac.class);
+            startActivity(intent);
+            autoCompleteTextView.setText("");
         } else if (search.equalsIgnoreCase("CEU History")) {
-
+            wv_faq_search.loadUrl("file:///android_asset/history_hamili.html");
+            autoCompleteTextView.setText("");
+        } else if (search.equalsIgnoreCase("CEU Music")) {
+            Intent intent = new Intent(getActivity(), ceuchoosemusic.class);
+            startActivity(intent);
+            autoCompleteTextView.setText("");
+        } else if (search.equalsIgnoreCase("El Collar De Sampaguita")) {
+            ceuchoosemusic.et_choosemusic.setText("elcollar");
+            Intent intent = new Intent(getActivity(), ceumusic.class);
+            startActivity(intent);
+            autoCompleteTextView.setText("");
+        } else if (search.equalsIgnoreCase("Sampaguita La Flor De Manila")) {
+            ceuchoosemusic.et_choosemusic.setText("laflor");
+            Intent intent = new Intent(getActivity(), ceumusic.class);
+            startActivity(intent);
+            autoCompleteTextView.setText("");
+        } else if (search.equalsIgnoreCase("Sampaguitang Mabango")) {
+            ceuchoosemusic.et_choosemusic.setText("mabango");
+            Intent intent = new Intent(getActivity(), ceumusic.class);
+            startActivity(intent);
+            autoCompleteTextView.setText("");
+        } else if (search.equalsIgnoreCase("CEU Hymn")) {
+            ceuchoosemusic.et_choosemusic.setText("hymn");
+            Intent intent = new Intent(getActivity(), ceumusic.class);
+            startActivity(intent);
+            autoCompleteTextView.setText("");
         } else if (search.equalsIgnoreCase("Serious Offense")) {
+            wv_faq_search.loadUrl("file:///android_asset/serious_offenses.html");
+            autoCompleteTextView.setText("");
 
         } else if (search.equalsIgnoreCase("Less Serious Offense")) {
-
+            wv_faq_search.loadUrl("file:///android_asset/less_serious_offenses.html");
+            autoCompleteTextView.setText("");
         } else if (search.equalsIgnoreCase("Slight Offense")) {
-
+            wv_faq_search.loadUrl("file:///android_asset/slight_offenses.html");
+            autoCompleteTextView.setText("");
         } else if (search.equalsIgnoreCase("Other Offenses")) {
-
+            wv_faq_search.loadUrl("file:///android_asset/other_offenses_and_sanctions.html");
+            autoCompleteTextView.setText("");
         } else if (search.equalsIgnoreCase("Freshmen Enrollment")) {
-
+            wv_faq_search.loadUrl("file:///android_asset/enrollment_freshmen.html");
+            autoCompleteTextView.setText("");
         } else if (search.equalsIgnoreCase("Alien/Non-Resident Enrollment")) {
-
+            wv_faq_search.loadUrl("file:///android_asset/enrollment_aliens_nonresident_ofw.html");
+            autoCompleteTextView.setText("");
         } else if (search.equalsIgnoreCase("Second Degree Enrollment")) {
-
+            wv_faq_search.loadUrl("file:///android_asset/enrollment_second_degree.html");
+            autoCompleteTextView.setText("");
         } else if (search.equalsIgnoreCase("Continuing Student Enrollment")) {
-
+            wv_faq_search.loadUrl("file:///android_asset/enrollment_continuing_students.html");
+            autoCompleteTextView.setText("");
         } else if (search.equalsIgnoreCase("Transferee Enrollment")) {
-
+            wv_faq_search.loadUrl("file:///android_asset/enrollment_transferees.html");
+            autoCompleteTextView.setText("");
         } else if (search.equalsIgnoreCase("Add and Drop")) {
-
+            wv_faq_search.loadUrl("file:///android_asset/add_drop_subjects.html");
+            autoCompleteTextView.setText("");
         } else if (search.equalsIgnoreCase("Cross Enroll")) {
-
+            wv_faq_search.loadUrl("file:///android_asset/cross_enroll.html");
+            autoCompleteTextView.setText("");
         } else if (search.equalsIgnoreCase("Evaluation for Graduating Students")) {
-
+            wv_faq_search.loadUrl("file:///android_asset/evaluation_for_grad.html");
+            autoCompleteTextView.setText("");
         } else if (search.equalsIgnoreCase("ID")) {
-
+            wv_faq_search.loadUrl("file:///android_asset/id.html");
+            autoCompleteTextView.setText("");
         } else if (search.equalsIgnoreCase("Promissory Note")) {
-
+            wv_faq_search.loadUrl("file:///android_asset/promissory_note.html");
+            autoCompleteTextView.setText("");
         } else if (search.equalsIgnoreCase("Re-Admission") || search.equalsIgnoreCase("Readmission")) {
-
+            wv_faq_search.loadUrl("file:///android_asset/readmission.html");
+            autoCompleteTextView.setText("");
         } else if (search.equalsIgnoreCase("Refund")) {
-
+            wv_faq_search.loadUrl("file:///android_asset/refund.html");
+            autoCompleteTextView.setText("");
         } else if (search.equalsIgnoreCase("Shifting")) {
-
+            wv_faq_search.loadUrl("file:///android_asset/shifting.html");
+            autoCompleteTextView.setText("");
         }
 
 
