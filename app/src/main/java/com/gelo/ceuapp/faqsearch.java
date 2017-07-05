b30841721216c8d81283cc5a3c4a0657f4f82e6b
Trends Angelo <gelodgreat@users.noreply.github.com>
2017-07-05 14:20:33 +0800

    Search

---
+++ app/src/main/java/com/gelo/ceuapp/faqsearch.java
@@ -0,0 +1,27 @@
+package com.gelo.ceuapp;
+
+import android.content.Context;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+public class faqsearch extends Fragment {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        final View view = inflater.inflate(R.layout.fragment_faqsearch, container, false);
+
+        return view;
+    }
+
+}
